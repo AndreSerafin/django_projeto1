b07432ad6abed3b4f90d91b0f72770546b6fff8b
Andre Pereira <andreserafim11@gmail.com>
2022-12-12 16:51:20 -0300

    html basico do site

--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'andre pereira',
         'age': 22,
         'height': 1.70,
     })
-
-
-def contato(request):
-    return HttpResponse('Sobre')
-
-
-def sobre(request):
-    return render(request, 'recipes/sobre.html')
