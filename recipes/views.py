cd18df7fb1452efdc465436566773f361d3d49ee
Andre Pereira <andreserafim11@gmail.com>
2022-12-14 11:58:54 -0300

    testes

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'andre pereira',
-        'age': 22,
-        'height': 1.70,
+        'recipes': [make_recipe() for _ in range(10)],
+    })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': 'True',
     })
